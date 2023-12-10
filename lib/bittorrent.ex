62667c7d92b5ceeb915261f0d5e84efe8103bb3a
Tiago Avila <tiagoa@corebridge.net>
2023-12-10 18:29:02 -0300

    Decode bencoded lists

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -21,6 +21,41 @@ defmodule Bittorrent.CLI do
 end
 
 defmodule Bencode do
+  def decode(<<"l", rest::binary>>) do
+    size = byte_size(rest) - 1
+    <<list_content::binary-size(size), "e"::binary>> = rest
+
+    Enum.map_reduce(
+      list_content |> String.graphemes() |> Enum.with_index(),
+      %{ignore_next: 0, string_acc: ""},
+      fn {char, index}, %{ignore_next: chars_count, string_acc: str_acc} ->
+        case char do
+          "e" when chars_count == 0 ->
+            possible_bencoded_integer = str_acc <> char
+
+
+            if Regex.match?(~r/i-*\d+e/, possible_bencoded_integer),
+              do: {decode(possible_bencoded_integer), %{ignore_next: 0, string_acc: ""}},
+              else: {"", %{ignore_next: 0, string_acc: possible_bencoded_integer}}
+
+          ":" ->
+            chars_count = str_acc |> String.to_integer()
+
+            possible_bencoded_string =
+              String.slice(list_content, index - 1, chars_count + 2)
+
+            {decode(possible_bencoded_string), %{ignore_next: chars_count, string_acc: ""}}
+
+          _ ->
+            if chars_count > 0,
+              do: {"", %{ignore_next: chars_count - 1, string_acc: ""}},
+              else: {"", %{ignore_next: 0, string_acc: str_acc <> char}}
+        end
+      end
+    )
+    |> then(fn {list, _} -> list |> Enum.filter(&(&1 != "")) end)
+  end
+
   def decode(<<"i", rest::binary>>) do
     rest
     |> String.replace("e", "")
---
+++ test/bittorrent_test.exs
@@ -0,0 +1,10 @@
+defmodule BittorrentTest do
+  use ExUnit.Case
+
+  test "decode bencoded lists" do
+    assert Bencode.decode("li52ee") == [52]
+    assert Bencode.decode("l5:helloe") == ["hello"]
+    assert Bencode.decode("l5:helloi52ee") == ["hello", 52]
+    assert Bencode.decode("li52e5:helloe") == [52, "hello"]
+  end
+end
