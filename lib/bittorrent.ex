dc05bfd842c964e45074133d278701865e46d4a6
Tiago Avila <tiagoa@corebridge.net>
2023-12-10 16:58:24 -0300

    pass 2nd stage

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -21,19 +21,22 @@ defmodule Bittorrent.CLI do
 end
 
 defmodule Bencode do
+  def decode(<<"i", rest::binary>>) do
+    rest
+    |> String.replace("e", "")
+    |> String.to_integer()
+  end
+
   def decode(encoded_value) when is_binary(encoded_value) do
     binary_data = :binary.bin_to_list(encoded_value)
 
-    with index <- Enum.find_index(binary_data, fn char -> char == 58 end),
-         is_bencoded_integers <- index == nil and Regex.match?(~r/i-*\d+e/, encoded_value) do
-      if is_bencoded_integers do
-        String.replace(encoded_value, ~r/i(-*\d+)e/, "\\1")
-      else
+    case Enum.find_index(binary_data, fn char -> char == 58 end) do
+      nil ->
+        IO.puts("The ':' character is not found in the binary")
+
+      index ->
         rest = Enum.slice(binary_data, (index + 1)..-1)
         List.to_string(rest)
-      end
-    else
-      _ -> IO.puts("The ':' character is not found in the binary")
     end
   end
 
